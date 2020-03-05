4c52e9afb8ea35162b1e814fb5c222ee1d1405c9
Alexander Trousevich <trousev@yandex.ru>
2020-03-05 18:08:15 +0300

    up

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
-#!/bin/bash -e
+#!/bin/bash
 set -x
 mkdir -p /workdir
 export RUNNER_ALLOW_RUNASROOT=1
 cd /actions-runner
 echo "Configuring it..."
 ./config.sh --url https://github.com/Visyond/visyond \
+    --unattended --replace \
     --token ${TOKEN} \
     --name visyond-linux-runner-`hostname` \
     --work /workdir
 echo "Running it..."
-./run.sh
+while [ true ]; do
+    ./run.sh
+    echo "Stopped. Restarting in 1 minute"
+    sleep 60
+done
